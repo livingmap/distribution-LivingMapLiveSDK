bd2e402045fe3d5c9dd04c7b825d0585efbda91a
Robin Macharg <robin.macharg+github@gmail.com>
2021-05-08 18:33:44 +0100

    Test of xcode 12.4 release

--- LivingMapLiveSDK.framework/Headers/LivingMapLiveSDK.h
+++ LivingMapLiveSDK.framework/Headers/LivingMapLiveSDK.h
@@ -1,4 +1,11 @@
-#import <UIKit/UIKit.h>
+//
+//  LivingMapLiveSDK.h
+//  LivingMapLiveSDK
+//
+//  Created by Robin Macharg2 on 08/05/2021.
+//
+
+#import <Foundation/Foundation.h>
 
 //! Project version number for LivingMapLiveSDK.
 FOUNDATION_EXPORT double LivingMapLiveSDKVersionNumber;
